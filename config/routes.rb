7ddac6624d0dfeffe94438e4688de0822b56abec
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-07-06 07:21:06 +0000

    [add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 生徒用
+  # URL /students/sign_in ...
+  devise_for :students, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
