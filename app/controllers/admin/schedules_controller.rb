8262a22145cd709c8df1e66c720cd8d9111d02b9
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-07-08 09:00:06 +0000

    [add]login,schedule

--- app/controllers/admin/schedules_controller.rb
+++ app/controllers/admin/schedules_controller.rb
@@ -1,13 +1,37 @@
 class Admin::SchedulesController < ApplicationController
   def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to admin_schedules_path, notice: "シフトを登録しました。"
+    else
+      render 'new'
+    end
   end
 
   def index
+    @schedules = Schedule.all
   end
 
   def show
+    @schedule = Schedule.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:start_time, :finish_time)
   end
 end
