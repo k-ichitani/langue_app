2bab72e630c7fea9492b2b654167754b2f719bbb
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-09-01 13:49:26 +0000

    [update]favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,14 @@ class FavoritesController < ApplicationController
   def create
     if student_signed_in?
       @favorite = Favorite.create(student_id: current_student.id, teacher_id: params[:id])
+      redirect_to teachers_information_path
     end
   end
 
   def destroy
     if student_signed_in?
       Favorite.find_by(student_id: current_student.id, teacher_id: params[:id]).destroy
+      redirect_to teachers_information_path
     end
   end
 end
