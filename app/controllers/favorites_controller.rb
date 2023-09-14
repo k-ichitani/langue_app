a1ef50d4fe38dfae4f1a296d74ceeb38b5571c04
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-09-14 23:46:08 +0000

    [update]favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,17 @@
 class FavoritesController < ApplicationController
   def create
     if student_signed_in?
+      @teacher = Teacher.find(params[:id])
       @favorite = Favorite.create(student_id: current_student.id, teacher_id: params[:id])
-      # redirect_to teachers_information_path
+      redirect_back fallback_location: root_path
     end
   end
 
   def destroy
     if student_signed_in?
+      @teacher = Teacher.find(params[:id])
       Favorite.find_by(student_id: current_student.id, teacher_id: params[:id]).destroy
-      # redirect_to teachers_information_path
+      redirect_back fallback_location: root_path
     end
   end
 end
